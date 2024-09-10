2b1913393ee5a8387f39ae808fad9f1fe16f1ee4
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-09-10 21:44:15 +0200

    inMemoryRepositories

---
+++ Server/RepositoryContracts/ICommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+
+namespace RepositoryContracts
+{
+    public interface ICommentRepository
+    {
+        Task<Comment> AddAsync(Comment comment);
+        Task UpdateAsync(Comment comment);
+        Task DeleteAsync(int id);
+        Task<Comment> GetSingleAsync(int id);
+        IQueryable<Comment> GetMany();
+    }
+}
---
+++ Server/RepositoryContracts/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+
+namespace RepositoryContracts
+{
+    public interface IUserRepository
+    {
+        Task<User> AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
+        Task<User> GetSingleAsync(int id);
+        IQueryable<User> GetMany();
+    }
+}
