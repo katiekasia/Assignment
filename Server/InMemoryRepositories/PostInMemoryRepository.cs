8df8f0adf5189119ab5d42f59deffdf72091935c
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-09-10 21:50:45 +0200

    InMemory

---
+++ Server/InMemoryRepositories/PostInMemoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories
+{
+    public class PostInMemoryRepository: IPostRepository
+    {
+        public List<Post> posts;
+        
+        public Task<Post> AddAsync(Post post)
+        {
+            post.Id = posts.Any() 
+                ? posts.Max(p => p.Id) + 1
+                : 1;
+            posts.Add(post);
+            return Task.FromResult(post);
+        }
+
+
+
+    }
+}
